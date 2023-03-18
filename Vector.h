314c4b31a4f3d6c372e87285eab23c3e3fd6e350
Ebrahem Mordy <106280874+therealebrahem@users.noreply.github.com>
2023-03-18 11:43:13 +0200

    Vector Code
    this is the code for Vector Structure

---
+++ Vector.h
@@ -0,0 +1,139 @@
+#ifndef VECTOR
+#define VECTOR
+using namespace std;
+class Vector{
+private:
+    int *arr{nullptr};
+    int size{};
+    int cap{};
+    void exCap(){
+        cap*=2;
+        int *b=new int[cap];
+        for(int i=0;i<size;i++) b[i]=arr[i];
+        swap(b,arr);
+        delete[] b;
+    };
+public:
+    Vector(int Size):size(Size){
+        if(size==0)
+            size=1;
+        cap=size*2;
+        arr=new int[cap];
+    }
+    ~Vector(){
+        delete[] arr;
+        arr= nullptr;
+        size=0;
+    }
+    void push(int x){
+        if(size==cap)
+            exCap();
+        int *b=new int[size+1];
+        for(int i=0;i<size;i++)
+            b[i]=arr[i];
+        b[size]=x;
+        swap(arr,b);
+        delete[] b;
+        b=nullptr;
+        size++;
+    }
+    void pop(){
+        if(empty())
+            return;
+        size--;
+    }
+    int find(int x){
+        for(int i=0;i<size;i++){
+            if(arr[i]==x)
+                return i+1;
+        }
+        return -1;
+    }
+    void print(){
+        for(int i=0;i<size;i++)
+            cout<<arr[i]<<" ";
+        cout<<endl;
+    }
+    void print_element(int idx){
+        cout<<arr[idx-1]<<endl;
+    }
+    void remove(int idx){
+        for(int i=idx-1;i<size-1;i++){
+            arr[i]=arr[i+1];
+        }
+        pop();
+    }
+    int Size(){
+        return size;
+    }
+    int Capacity(){
+        return cap;
+    }
+    int front(){
+        return arr[0];
+    }
+    int back(){
+        return arr[size-1];
+    }
+    void clear(){
+        size=0;
+    }
+    int get(int idx){
+        return arr[idx-1];
+    }
+    void set(int x,int idx){
+        arr[idx-1]=x;
+    }
+    void insert(int x,int idx){
+        if(size==cap)
+            exCap();
+        int *b=new int[size+1];
+        for(int i=0;i<idx-1;i++)
+            b[i]=arr[i];
+        b[idx-1]=x;
+        for(int i=idx;i<=size;i++)
+            b[i]=arr[i-1];
+        swap(arr,b);
+        delete[] b;
+        b=nullptr;
+        size++;
+    }
+    bool empty(){
+        if(size==0)
+            return true;
+        return false;
+    }
+    void right_rotate(){
+        int x=arr[size-1];
+        for(int i=size-1;i>=1;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=x;
+    }
+    void right_rotate(int k){
+        k%=size;
+        while(k--){
+            int x=arr[size-1];
+            for(int i=size-1;i>=1;i--){
+                arr[i]=arr[i-1];
+            }
+            arr[0]=x;
+        }
+    }
+    void left_rotate(){
+        int x=arr[0];
+        for(int i=0;i<size-1;i++){
+            arr[i]=arr[i+1];
+        }
+        arr[size-1]=x;
+    };
+    int pop(int idx){
+        int x=arr[idx-1];
+        for(int i=idx-1;i<size-1;i++){
+            arr[i]=arr[i+1];
+        }
+        pop();
+        return x;
+    }
+};
+#endif //VECTOR
