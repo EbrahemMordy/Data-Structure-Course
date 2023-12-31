75440d3a6ee8ffe7d070de27cb7cbe5d47d3f05e
Ebrahem Mordy <106280874+therealebrahem@users.noreply.github.com>
2023-12-31 09:21:45 +0200

    Update 1- Vector.h

--- 1- Vector.h
+++ 1- Vector.h
@@ -1,107 +1,127 @@
 #ifndef VECTOR
 #define VECTOR
 using namespace std;
-class Vector{
+
+class Vector {
 private:
     int *arr{nullptr};
     int size{};
     int cap{};
-    void exCap(){
-        cap*=2;
-        int *b=new int[cap];
-        for(int i=0;i<size;i++) b[i]=arr[i];
-        swap(b,arr);
+
+    void exCap() {
+        cap *= 2;
+        int *b = new int[cap];
+        for (int i = 0; i < size; i++) b[i] = arr[i];
+        swap(b, arr);
         delete[] b;
+        b = nullptr;
     };
 public:
-    Vector(int Size):size(Size){
-        if(size==0)
-            size=1;
-        cap=size*2;
-        arr=new int[cap];
+    Vector(int Size) : size(Size) {
+        if (size == 0)
+            size = 1;
+        cap = size * 2;
+        arr = new int[cap];
     }
-    ~Vector(){
+
+    ~Vector() {
         delete[] arr;
-        arr= nullptr;
-        size=0;
+        arr = nullptr;
+        size = 0;
     }
-    void push(int x){
-        if(size==cap)
+
+    void push(int x) {
+        if (size == cap)
             exCap();
-        int *b=new int[size+1];
-        for(int i=0;i<size;i++)
-            b[i]=arr[i];
-        b[size]=x;
-        swap(arr,b);
-        delete[] b; 
-        b=nullptr;
+        int *b = new int[size + 1];
+        for (int i = 0; i < size; i++)
+            b[i] = arr[i];
+        b[size] = x;
+        swap(arr, b);
+        delete[] b;
+        b = nullptr;
         size++;
     }
-    void pop(){
-        if(empty())
+
+    void pop() {
+        if (empty())
             return;
         size--;
     }
-    int find(int x){
-        for(int i=0;i<size;i++){
-            if(arr[i]==x)
+
+    int find(int x) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == x)
                 return i;
         }
         return -1;
     }
-    void print(){
-        for(int i=0;i<size;i++)
-            cout<<arr[i]<<" ";
-        cout<<endl;
+
+    void print() {
+        for (int i = 0; i < size; i++)
+            cout << arr[i] << " ";
+        cout << endl;
     }
-    void print_element(int idx){
-        cout<<arr[idx-1]<<endl;
+
+    void print_element(int idx) {
+        cout << arr[idx - 1] << endl;
     }
-    void remove(int idx){
-        for(int i=idx;i<size-1;i++){
-            arr[i]=arr[i+1];
+
+    void remove(int idx) {
+        for (int i = idx; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
         }
         pop();
     }
-    int Size(){
+
+    int Size() {
         return size;
     }
-    int Capacity(){
+
+    int Capacity() {
         return cap;
     }
-    int front(){
+
+    int front() {
         return arr[0];
     }
-    int back(){
-        return arr[size-1];
+
+    int back() {
+        return arr[size - 1];
     }
-    void clear(){
-        size=0;
+
+    void clear() {
+        size = 0;
     }
-    int get(int idx){
+
+    int get(int idx) {
         return arr[idx];
     }
-    void set(int x,int idx){
-        arr[idx]=x;
+
+    void set(int x, int idx) {
+        arr[idx] = x;
     }
-    void insert(int x,int idx){
-        if(size==cap)
+
+    void insert(int x, int idx) {
+        if (size == cap)
             exCap();
-        int *b=new int[size+1];
-        for(int i=0;i<idx;i++)
-            b[i]=arr[i];
-        b[idx]=x;
-        for(int i=idx+1;i<=size;i++)
-            b[i]=arr[i-1];
-        swap(arr,b);
+        int *b = new int[size + 1];
+        for (int i = 0; i < idx; i++)
+            b[i] = arr[i];
+        b[idx] = x;
+        for (int i = idx + 1; i <= size; i++)
+            b[i] = arr[i - 1];
+        swap(arr, b);
         delete[] b;
-        b=nullptr;
+        b = nullptr;
         size++;
     }
-    bool empty(){
-        if(size==0)
+
+    bool empty() {
+        if (size == 0)
             return true;
         return false;
     }
 };
+
 #endif //VECTOR
